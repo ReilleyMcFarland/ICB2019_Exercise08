d1edc29e69e6e6c28b8089718165112206dd0afc
Reilley Knott <rknott@nd.edu>
2019-11-21 15:04:13 -0500

    Part 2 finished

--- Exercise8.R
+++ Exercise8.R
@@ -51,8 +51,41 @@ plot(UWTiming,UWRunningTotal,type="l",xlab="Time",ylab="Score",col="red")
 lines(MSUTiming,MSURunningTotal,type="l",col="dark green")
 
 #Part2
-#initialize the vector with 
+#initialize the vector with numbers 1-100
 numbers<-c({1:100})
+
+#initialize the random number, between 1 and 100. It will be a different number every game play
+RNumber<-sample(numbers,1)
+
+#initialize the game
+print("I'm thinking of a number between 1 and 100... You have 10 guesses")
+#set that the user gets only 10 guesses
+for (i in {1:11}) {
+  #losing condition
+  if (i == 11) {
+    cat(sprintf("You lose! The number was %i.",RNumber))
+    #game over
+    break
+  }
+  #asking the user to input their guess
+  guess<-readline(prompt="Your guess: ")
+  #the guess is greater than the number
+  if(guess > RNumber) {
+    print("Lower")
+    #the guess is lower than the number
+  } else if (guess < RNumber) {
+    print("Higher")
+    #the user guesses the number correctly
+  } else if (guess == RNumber) {
+    cat(sprintf("Correct! The number was %i!",RNumber))
+    #game ends
+    break
+  } 
+}
+
+
+
+
 
 
 
